25400bc7eaa7be6cfe07be0f2f58c63f5e0f61e2
Bing Rao <bing.rao@outlook.com>
2020-04-05 02:32:22 -0400

    update transform adapters

--- asm_study/src/main/scala/org/example/transfrom/TransformAdapter.scala
+++ asm_study/src/main/scala/org/example/transfrom/TransformAdapter.scala
@@ -3,8 +3,21 @@ package org.example.transfrom
 import org.example.utils.Common
 import org.objectweb.asm.{ClassVisitor, Opcodes}
 
-class TransformAdapter(writer:ClassVisitor)
-  extends ClassVisitor(Opcodes.V1_8, writer)
+class TransformAdapter(cv:ClassVisitor)
+  extends ClassVisitor(Opcodes.V1_8, cv)
     with Common{
-
+  /**
+   *
+   * This class overrides only one method of the ClassVisitor class. As a consequence
+   * all calls are forwarded unchanged to the class visitor cv passed to the
+   * constructor, except calls to the visit method, which are forwarded with a
+   * modified class version number.
+   */
+  override def visit(version: Int,
+                     access: Int,
+                     name: _root_.java.lang.String,
+                     signature: _root_.java.lang.String,
+                     superName: _root_.java.lang.String,
+                     interfaces: Array[_root_.java.lang.String]): Unit =
+    cv.visit(Opcodes.V1_7, access, name, signature, superName, interfaces)
 }
